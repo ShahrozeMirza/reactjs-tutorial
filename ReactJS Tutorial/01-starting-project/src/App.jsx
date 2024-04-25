135d086a0e8db264616b7e5a108d927aa9f67206
Mirza Muhammad Ali Shahroze <ali.shahroze@confiz.com>
2024-04-26 02:51:59 +0500

    React tutorials continued

--- ReactJS Tutorial/01-starting-project/src/App.jsx
+++ ReactJS Tutorial/01-starting-project/src/App.jsx
@@ -4,6 +4,11 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+
+  function handleSelect() {
+    console.log('Hello World - Selected!')
+}
+
   return (
     <div>
       <Header />
@@ -33,11 +38,12 @@ function App() {
           <menu>
             {/* This way of building components where our components can wrap other components or other 
             content is called component composition */}
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
+          Dynamic Content
 
         </section>
        
--- ReactJS Tutorial/01-starting-project/src/components/TabButton.jsx
+++ ReactJS Tutorial/01-starting-project/src/components/TabButton.jsx
@@ -2,12 +2,16 @@
 not entirely empty. Instead there is one prop which we will always get, the special built-in children prop. This is a prop that is set by react and it's a prop that is not set with the help
 of attributes, instead this children prop simply refers to the content between your component tags. */
 
+/* The advantage of defining event handler functions inside the component function is that they have access to the component's props and state */
+
+
+
 
 /* export default function TabButton(props) {
     return <li><button>{props.children}</button></li>
 */
 
 //With object destructuring and by pulling out the children property and storing it in its own variable we don't need to write prop.children.
-export default function TabButton({children}) {
-    return <li><button>{children}</button></li>;
+export default function TabButton({children, onSelect}) {
+    return <li><button onClick={onSelect}>{children}</button></li>;
 }
