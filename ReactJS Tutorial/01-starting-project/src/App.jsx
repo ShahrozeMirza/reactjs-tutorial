d718cbf4ca406def885ff7f1666c30eb6d372ba2
Mirza Muhammad Ali Shahroze <ali.shahroze@confiz.com>
2024-05-08 00:48:26 +0500

    tutorials continued

--- ReactJS Tutorial/01-starting-project/src/App.jsx
+++ ReactJS Tutorial/01-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
@@ -5,9 +6,20 @@ import TabButton from './components/TabButton.jsx';
 
 function App() {
 
-  function handleSelect() {
-    console.log('Hello World - Selected!')
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {
+    //selectedButton => 'components', 'JSX', 'props', 'state'
+    setSelectedTopic(selectedButton);
+
+    //Here the old value will be shown in console because we are inside the function which has old value of selectedTopic before re-render.
+    console.log('old value inside', selectedTopic);
 }
+
+//Here the new value will be shown in console because we are outside the function which has new value of selectedTopic after re-render.
+console.log('new value outside', selectedTopic);
+
+console.log('APP COMPONENT EXECUTING')
 
   return (
     <div>
@@ -38,12 +50,12 @@ function App() {
           <menu>
             {/* This way of building components where our components can wrap other components or other 
             content is called component composition */}
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('States')}>State</TabButton>
           </menu>
-          Dynamic Content
+         { selectedTopic }
 
         </section>
        
